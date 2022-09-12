f11dbaea1fbce66dd9d38fbcdda2cac9fc2d5d51
black-mamba73 <j.n.orihito.111@gmail.com>
2022-09-12 15:50:03 +1000

    Update hooks.py

--- ssg/hooks.py
+++ ssg/hooks.py
@@ -1,5 +1,6 @@
 _callbacks = {}
 
+
 def register(hook, order=0):
     def register_callback(func):
         _callbacks.setdefault(hook, {}).setdefault(order, []).append(func)
@@ -11,6 +12,7 @@ def event(hook, *args):
     for order in sorted(_callbacks.get(hook, {})):
         for func in _callbacks[hook][order]:
             func(*args)
+
 
 def filter(hook, value, *args):
     for order in sorted(_callbacks.get(hook, {})):
